6739e133b90c0e4bef2b52890c8a8c1d4962493d
Theristes <theristes@gmail.com>
2025-02-18 12:13:00 -0300

    clean opencart

--- install/opencart.sql
+++ install/opencart.sql
@@ -775,6 +775,7 @@ INSERT INTO `oc_setting` (`store_id`, `code`, `key`, `value`, `serialized`) VALU
 (0, 'report_marketing', 'report_marketing_status', '1', 0),
 (0, 'report_marketing', 'report_marketing_sort_order', '12', 0),
 (0, 'developer', 'developer_theme', '1', 0),
+
 (0, 'developer', 'developer_sass', '1', 0);
 
 -----------------------------------------------------------
