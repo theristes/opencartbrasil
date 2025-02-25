37fe4be3cc3ebf8fdd2d41a2eb5d920ff36553f9
Theristes <theristes@gmail.com>
2025-02-25 19:54:42 -0300

    switching code language to 2

--- catalog/controller/startup/startup.php
+++ catalog/controller/startup/startup.php
@@ -39,7 +39,7 @@ class ControllerStartupStartup extends Controller {
 		$this->registry->set('url', new Url($this->config->get('config_url'), $this->config->get('config_ssl')));
 
 		// Language
-		$code = '';
+		$code = '2';
 
 		$this->load->model('localisation/language');
 
